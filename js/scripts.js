e1de99a391598ddcf6bb456c21e20a9518348d6c
Haneen AbuKhater <abukhater.haneen@gmail.com>
2017-07-21 11:12:04 -0700

    add prototype method to calculate the total cost with styling changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,32 @@
-$(document).ready(function(){
+function PizzaOrder(sauce, size, topping, side){
+  this.pSauce= sauce;
+  this.pSize= size;
+  this.pTopping = topping;
+  this.pSide = side;
+}
+PizzaOrder.prototype.pizzaCost = function() {
+  var total = (this.pSize + this.pTopping + this.pSide);
+  return total;
+}
 
+$(document).ready(function(){
+  $("#pizzaForm").submit(function(event){
+    var sauce = $("#pizza").val();
+    var size = parseInt($("input:radio[name=size]:checked").val());
+    var side = parseInt($("input:radio[name=side]:checked").val());
+    var toppings=0;
+    $("input:checkbox[name=topping]:checked").each(function(){
+      toppings += parseInt($(this).val());
+    });
+
+    var newPizza = new PizzaOrder(sauce, size, toppings, side);
+    console.log(newPizza);
+
+    $(".receipt").show();
+    $("#pizzaStyle").text(sauce);
+    $("#cost").text(newPizza.pizzaCost());
+
+
+    event.preventDefault();
+  });
 });
