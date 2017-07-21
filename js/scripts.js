64384cc84fd02eba46e72b4a340adf9a2e5ec2ac
Haneen AbuKhater <abukhater.haneen@gmail.com>
2017-07-21 15:26:15 -0700

    add functionality to add all pizza orders for total cost

--- js/scripts.js
+++ js/scripts.js
@@ -3,30 +3,35 @@ function PizzaOrder(sauce, size, topping, side){
   this.pSize= size;
   this.pTopping = topping;
   this.pSide = side;
+  this.finalCost =0;
 }
 PizzaOrder.prototype.pizzaCost = function() {
   var total = (this.pSize + this.pTopping + this.pSide);
   return total;
 }
-
 $(document).ready(function(){
+  $("#add-pizza").click(function(){
+    $('input[type=checkbox]').each(function() {this.checked=false});
+  });
+  var total =[];
   $("#pizzaForm").submit(function(event){
     var sauce = $("#pizza").val();
     var size = parseInt($("input:radio[name=size]:checked").val());
     var side = parseInt($("input:radio[name=side]:checked").val());
     var toppings=0;
+    var finalPrice =0;
     $("input:checkbox[name=topping]:checked").each(function(){
       toppings += parseInt($(this).val());
     });
-
     var newPizza = new PizzaOrder(sauce, size, toppings, side);
-    console.log(newPizza);
-
-    $(".receipt").show();
+    total.push(newPizza.pizzaCost());
+    for(var i=0; i< total.length;i++){
+      finalPrice += total[i];
+    }
+    $(".receipt").fadeIn(2000);
     $("#pizzaStyle").text(sauce);
-    $("#cost").text(newPizza.pizzaCost());
-
-
+    $("#cost").text(finalPrice);
+    $('ul#morePizza').append('<li><h3><span class="pizza">'+ 'Pizza Cost: $' + newPizza.pizzaCost() + "</span></h3></li>");
     event.preventDefault();
   });
 });
